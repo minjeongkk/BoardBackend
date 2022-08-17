48aa9c71f09861bbf2130e5e06cf046c07a49e06
minjeongkk <kunvx@naver.com>
2022-08-17 18:44:20 +0900

    데이터 수정하기

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO article(id, title, content) VALUES (1, 'KIM', '1111');
+INSERT INTO article(id, title, content) VALUES (2, 'SKSKSKSK', 'HELLO~~~');
+INSERT INTO article(id, title, content) VALUES (3, 'Minjeong', 'OHOHOHOHOH!!!');
