bd6f81ecc118aed9b10d7928f9bc41b8ded13c42
minjeongkk <kunvx@naver.com>
2022-08-28 21:44:59 +0900

    AOP

--- src/main/java/com/example/firstproject/service/CommentService.java
+++ src/main/java/com/example/firstproject/service/CommentService.java
@@ -5,6 +5,7 @@ import com.example.firstproject.entity.Article;
 import com.example.firstproject.entity.Comment;
 import com.example.firstproject.repository.ArticleRepository;
 import com.example.firstproject.repository.CommentRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 public class CommentService {
     @Autowired
@@ -57,7 +59,7 @@ public class CommentService {
         Comment created = commentRepository.save(comment);
 
         // DTO로 변경하여 반환
-        return CommentDto.createCommentDto(created);
+         return CommentDto.createCommentDto(created);
     }
 
     @Transactional
