b38a1d79f08ff9cb14a7ceeab3902879e3eadbb4
minjeongkk <kunvx@naver.com>
2022-08-28 22:11:27 +0900

    ObjectMapper

--- src/main/java/com/example/firstproject/aop/DebuggingAspect.java
+++ src/main/java/com/example/firstproject/aop/DebuggingAspect.java
@@ -13,8 +13,8 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class DebuggingAspect {
 
-    // 대상 메소드 선택 : CommentService#create()
-    @Pointcut("execution(* com.example.firstproject.service.CommentService.*(..))")
+    // 대상 메소드 선택 : api 패키지의 모든 메소드
+    @Pointcut("execution(* com.example.firstproject.api.*.*(..))")
     private void cut() {}
 
     // 실행 시점 설정 : cut()의 대상이 수행되기 이전
